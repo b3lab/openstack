42b2a05b9996b0961f773482c5d4c889358e52fa
Gökhan IŞIK <gokhan.isik@tubitak.gov.tr>
2017-02-10 10:07:10 +0300

    Heat is updated

--- installation_scripts/controller/scripts/90_install_heat.sh
+++ installation_scripts/controller/scripts/90_install_heat.sh
@@ -18,13 +18,74 @@ sed -i -e "s/HEAT_DOMAIN_PASS/$HEAT_DOMAIN_PASS/g" $CONFIG_FILE
 
 
 
+mysql -u root -p$DB_PASS < ./sql_scripts/heat.sql
 
+
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=$ADMIN_PASS
+export OS_AUTH_URL=http://$controller_node_hostname:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+
+
+expect -c '
+  spawn openstack user create --domain default --password-prompt heat
+  expect "*?assword:*"
+  send "'"$HEAT_PASS"'\r";
+  expect "*?assword:*"
+  send "'"$HEAT_PASS"'\r";
+  interact '
+
+openstack role add --project service --user heat admin
+
+openstack service create --name heat --description "Orchestration" orchestration
+openstack service create --name heat-cfn --description "Orchestration"  cloudformation
+
+openstack endpoint create --region RegionOne orchestration public http://$controller_node_hostname:8004/v1/%\(tenant_id\)s
+
+openstack endpoint create --region RegionOne orchestration internal http://$controller_node_hostname:8004/v1/%\(tenant_id\)s
+
+openstack endpoint create --region RegionOne orchestration admin http://$controller_node_hostname:8004/v1/%\(tenant_id\)s
+
+openstack endpoint create --region RegionOne cloudformation public http://$controller_node_hostname:8000/v1
+
+openstack endpoint create --region RegionOne cloudformation internal http://$controller_node_hostname:8000/v1
+
+openstack endpoint create --region RegionOne cloudformation admin http://$controller_node_hostname:8000/v1
+
+openstack domain create --description "Stack projects and users" heat
+
+expect -c '
+  spawn openstack user create --domain heat --password-prompt heat_domain_admin
+  expect "*?assword:*"
+  send "'"$HEAT_DOMAIN_PASS"'\r";
+  expect "*?assword:*"
+  send "'"$HEAT_DOMAIN_PASS"'\r";
+  interact '
+
+openstack role add --domain heat --user-domain heat --user heat_domain_admin admin
+
+openstack role create heat_stack_owner
+
+openstack role add --project demo --user demo heat_stack_owner
+
+openstack role create heat_stack_user
+
+apt  install -y heat-api heat-api-cfn heat-engine
 
 cp ./conf_files/heat.conf /etc/heat/heat.conf
 chown heat:heat /etc/heat/heat.conf
 chmod 640 /etc/heat/heat.conf
 
+su -s /bin/sh -c "heat-manage db_sync" heat
 
+
+service heat-api restart
+service heat-api-cfn restart
+service heat-engine restart
 
 set +x
 
--- installation_scripts/controller/scripts/sql_scripts/heat.sql
+++ installation_scripts/controller/scripts/sql_scripts/heat.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE heat;
 
-GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'localhost' IDENTIFIED BY 'heatdbpass';
-GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'%' IDENTIFIED BY 'heatdbpass';
+GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'localhost' IDENTIFIED BY 'HEAT_DBPASS';
+GRANT ALL PRIVILEGES ON heat.* TO 'heat'@'%' IDENTIFIED BY 'HEAT_DBPASS';
 
 
