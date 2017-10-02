1f6bd9179af95879275fca0571cd0879a00fc588
celik.esra <celik.esra@tubitak.gov.tr>
2017-10-02 11:37:24 +0300

    Compute node installation scripts are updated for OpenStack Ocata version

--- installation_scripts/compute/scripts/20_install_nova_compute.sh
+++ installation_scripts/compute/scripts/20_install_nova_compute.sh
@@ -11,6 +11,7 @@ CONFIG_FILE=./conf_files/nova.conf
 sed -i -e "s/CONTROLLER_HOSTNAME/$controller_node_hostname/g" $CONFIG_FILE
 sed -i -e "s/MANAGEMENT_IP/$my_management_ip/g" $CONFIG_FILE
 sed -i -e "s/NOVA_PASS/$NOVA_PASS/g" $CONFIG_FILE
+sed -i -e "s/PLACEMENT_PASS/$PLACEMENT_PASS/g" $CONFIG_FILE
 sed -i -e "s/RABBIT_PASS/$RABBIT_PASS/g" $CONFIG_FILE
 sed -i -e "s/NEUTRON_PASS/$NEUTRON_PASS/g" $CONFIG_FILE
 
--- installation_scripts/compute/scripts/conf.sh
+++ installation_scripts/compute/scripts/conf.sh
@@ -8,14 +8,15 @@ DEMO_PASS=
 GLANCE_PASS=
 NEUTRON_PASS=
 NOVA_PASS=
+PLACEMENT_PASS=
 RABBIT_PASS=
 METADATA_SECRET=
 TUNNEL_IP=''
-controller_node_hostname=''
-controller_node_management_ip=''
+controller_node_hostname='<controller_node_hostname>'
+controller_node_management_ip='<<controller_node_management_ip>'
 
-my_hostname=''
-my_management_ip=''
+my_hostname='<my_hostname>'
+my_management_ip='<my_management_ip>'
 
 # we need 2 network interfaces, one for management network second for provider network
-provider_network_interface=''
+provider_network_interface='<provider_network_interface>'
